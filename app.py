5dc10586244a002694726c3156956b2f2ddd361d
rafaelmm12 <74003137+rafaelmm12@users.noreply.github.com>
2026-03-15 20:24:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain
 
 
 # ----------------------- App Configuration -----------------------
